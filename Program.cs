6dee889b2fbffc290cbcbc5d8db5c013f9b7f0dc
JoseFabianTeoSandoval <jteos1@miumg.edu.gt>
2024-04-06 10:48:22 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Clase_6_de_abril.Clases;
+
+Carro Carrito = new Carro("Toyota", 2024, "Rojo");
+Carro Carro2 = new Carro("Nissan", 2020, "Azul");
+
+Carrito.Color = "Rojo";
+Carrito.Owner = "Juan";
+Carrito.MAXVELOCIDAD = 200;
+
+Console.WriteLine($"El Carro es un {Carrito.Marca} ");
+Console.WriteLine($"El Carro es un {Carro2.Marca} ");
+
+Console.WriteLine(Carrito.Acelerar() + "/Kph");
+Console.WriteLine(Carrito.Acelerar() + "/Kph");
+Console.WriteLine(Carrito.Acelerar() + "/Kph");
+
+
